c6b1f04efbda4609793bd10365d9ad74321e1d56
columbooster <kinglove0403@gmail.com>
2022-06-13 23:01:03 +0900

    aop

--- src/main/java/com/example/aop/controller/RestApiController.java
+++ src/main/java/com/example/aop/controller/RestApiController.java
@@ -9,15 +9,11 @@ public class RestApiController {
 
     @GetMapping("/get/{id}")
     public String get(@PathVariable Long id, @RequestParam String name){
-        System.out.println("get method");
-        System.out.println("get method : " + id);
-        System.out.println("get method : " + name);
         return id + " " + name;
     }
 
     @PostMapping("/post")
     public User post(@RequestBody User user){
-        System.out.println("post method : " + user);
         return user;
     }
 
